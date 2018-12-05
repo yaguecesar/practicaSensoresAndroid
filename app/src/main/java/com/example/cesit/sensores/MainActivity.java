a645bc0a3b7c245c71c39490a0d1c6fd9e8b1d77
César Yagüe <yague.cesar@gmail.com>
2018-12-05 18:59:20 +0100

    Versión para entregar comentada y con manejo adecuado de mensajes mostrados

--- app/src/main/java/com/example/cesit/sensores/MainActivity.java
+++ app/src/main/java/com/example/cesit/sensores/MainActivity.java
@@ -8,33 +8,71 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Formatter;
 
 /**
  * TODO (1): Obtener datos de humedad
  * TODO (2): Obtener datos de temperatura
  * TODO (3): Obtener datos de un tercer sensor (¿referencia acelerómetro?)
+ * TODO (4): Añadir botones para activar y desactivar los distintos sensores utilizados
  *
  * NOTAS: desactivar todos los sensores que no se vayan a utilizar, especialmente en onPause, para no drenar rápidamente la batería
  */
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity implements SensorEventListener, View.OnClickListener{
 
-    public static final String TITULO_ALERTA_DE_TEMPERATURA = "Alerta de temperatura";
-    public static final String TITULO_ALERTA_DE_HUMEDAD = "Alerta de humedad";
-    public static final String MENSAJE_TEMPERATURA = "La temperatura no es la adecuada";
-    public static final String MENSAJE_HUMEDAD = "La humedad no es la adecuada";
+
+    // Valores límite para los distintos sensores
+    private static final float MAX_TEMP = 23;
+    private static final float MIN_TEMP = 20;
+    private static final float MIN_HUMEDAD = 40;
+    private static final float MAX_HUMEDAD = 60;
+    private static final int MIN_ILUMINACION = 10;
+    private static double LIMITE_ACELERACION = 30;
+
+    // SensorManager y sensores necesarios
     private SensorManager sensorManager;
     private Sensor humedad;
     private Sensor temperatura;
-    private Sensor giroscopio;
+    private Sensor acelerometro;
     private Sensor iluminacion;
-    private TextView txtTemperatura;
-    private TextView txtGiroscopio;
-    private TextView txtHumedad;
+
+    // TextViews para mostrar algunos valores
+    private TextView txtAcelerometro;
     private TextView txtIluminacion;
+
+    // AlertBuilder para los diálogos de alerta
     private AlertDialog.Builder alertBuilder;
-    private boolean mostrandoAlerta = false;
+
+    // Garantiza, mediante comprobaciones, que sólo se muestre una alerta
+    private boolean mostrandoAlerta;
+
+    // Botones para activar o desactivar el seguimiento de los sensores
+    private Button btnIluminacion;
+    private Button btnTemperatura;
+    private Button btnHumedad;
+    private Button btnAcelerometro;
+
+    // Estado del seguimiento de los sensores
+    private boolean iluminacionOn;
+    private boolean acelerometroOn;
+    private boolean temperaturaOn;
+    private boolean humedadOn;
+
+    // Variables para las pruebas con el acelerómetro
+    private Button btnLimite;
+    private EditText editLimite;
+    double maxVelocidad = 0;
+    private boolean modoPrueba = false;
 
 
     @Override
@@ -42,17 +80,35 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtTemperatura = findViewById(R.id.txtTemperatura);
-        txtGiroscopio = findViewById(R.id.txtGiroscopio);
-        txtHumedad = findViewById(R.id.txtHumedad);
+        mostrandoAlerta = false;
+        iluminacionOn = true;
+        humedadOn = true;
+        acelerometroOn = true;
+        temperaturaOn = true;
+
+        editLimite = findViewById(R.id.inputLimite);
+        btnLimite = findViewById(R.id.btnLimite);
+        btnLimite.setOnClickListener(this);
+
+        txtAcelerometro = findViewById(R.id.txtAcelerometro);
         txtIluminacion = findViewById(R.id.txtIluminacion);
+
+        btnAcelerometro = findViewById(R.id.btnAcelerometro);
+        btnHumedad = findViewById(R.id.btnHumedad);
+        btnTemperatura = findViewById(R.id.btnTemperatura);
+        btnIluminacion = findViewById(R.id.btnIluminacion);
+
+        btnAcelerometro.setOnClickListener(this);
+        btnIluminacion.setOnClickListener(this);
+        btnHumedad.setOnClickListener(this);
+        btnTemperatura.setOnClickListener(this);
 
         alertBuilder = new AlertDialog.Builder(this);
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         humedad = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
         temperatura = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
-        giroscopio = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        acelerometro = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         iluminacion = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
 //        List<Sensor> deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
 
@@ -61,7 +117,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onResume(){
         super.onResume();
         sensorManager.registerListener(this, temperatura, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(this, giroscopio, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, acelerometro, SensorManager.SENSOR_DELAY_NORMAL);
         sensorManager.registerListener(this, humedad, SensorManager.SENSOR_DELAY_NORMAL);
         sensorManager.registerListener(this, iluminacion, SensorManager.SENSOR_DELAY_NORMAL);
     }
@@ -70,34 +126,71 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onPause(){
         super.onPause();
         sensorManager.unregisterListener(this);
-        sensorManager.registerListener(this, temperatura, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (event.sensor == giroscopio){
-            txtGiroscopio.setText("Giroscopio: ");
-            txtGiroscopio.append(event.values[0] + "");
+
+
+
+        if (event.sensor == acelerometro){
+
+            // alpha is calculated as t / (t + dT)
+            // with t, the low-pass filter's time-constant
+            // and dT, the event delivery rate
+            double x = 0, y = 0, z = 0;
+
+            long tiempo = 1;
+
+
+            x = event.values[0];
+            y = event.values[1];
+            z = event.values[2];
+
+            // Módulo del vector aceleración
+            double speed = Math.sqrt(x*x+y*y+z*z);
+
+            if (speed > maxVelocidad){
+                maxVelocidad = speed;
+            }
+
+            if (speed > LIMITE_ACELERACION) {
+                mostrarAlerta("", getString(R.string.caida_msg));
+            }
+
+            Formatter f = new Formatter();
+            f.format("maxVel:%.4f\nLimite: %.4f", maxVelocidad, LIMITE_ACELERACION);
+
+            String textoGiroscopio = f.toString();
+            txtAcelerometro.setText(textoGiroscopio);
         }
 
         if (event.sensor == temperatura) {
-            txtTemperatura.setText("Temperatura: ");
-            for (float valor : event.values) {
-                txtTemperatura.append(valor + "\n");
+
+            if (event.values[0] > MAX_TEMP){
+                mostrarAlerta("", getString(R.string.temp_alta_msg));
+            }else{
+                if (event.values[0] < MIN_TEMP){
+                    mostrarAlerta("", getString(R.string.temp_baja_msg));
+                }
             }
-            // mostrarAlerta("Temperatura", "La temperatura es ");
         }
 
         if (event.sensor == humedad){
-            txtHumedad.setText("Humedad: ");
-            txtHumedad.append(event.values[0] + "");
+            if (event.values[0] > MAX_HUMEDAD){
+                mostrarAlerta("", getString(R.string.humedad_alta_msg));
+            }else{
+                if (event.values[0] < MIN_HUMEDAD){
+                    mostrarAlerta("", getString(R.string.humedad_baja_msg));
+                }
+            }
         }
 
         if (event.sensor == iluminacion){
             txtIluminacion.setText("Iluminación: ");
             txtIluminacion.append(event.values[0] + "");
-            if (event.values[0] < 10){
-                mostrarAlerta("luz", "La iluminación ha bajado de 10");
+            if (event.values[0] < MIN_ILUMINACION){ // AlertaLuz se ha introducido para que no se muestre más de un diágolo, si se elimina la comprobación aparecerán varios diálogos al producirse cambios en los sensores (que es cuando mostrarAlerta es llamada
+                mostrarAlerta("luz", getString(R.string.iluminacion_baja_msg));
             }
         }
     }
@@ -108,13 +201,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     private void mostrarAlerta(String titulo, String mensaje){
-
-        // Esta variable se ha introducido para que no se muestre más de un diágolo, si se elimina la comprobación aparecerán varios diálogos al producirse cambios en los sensores (que es cuando mostrarAlerta es llamada
-        if (!mostrandoAlerta){
-            alertBuilder.setTitle(titulo);
-            alertBuilder.setTitle(mensaje);
-            alertBuilder.setCancelable(true);
-
+        if (!mostrandoAlerta) {
             alertBuilder.setNegativeButton(R.string.dialog_OK, new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
@@ -122,10 +209,89 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     mostrandoAlerta = false;
                 }
             });
+            alertBuilder.setTitle(titulo);
+            alertBuilder.setTitle(mensaje);
+            alertBuilder.setCancelable(true);
 
-            alertBuilder.show();
             mostrandoAlerta = true;
+            alertBuilder.show();
         }
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnHumedad:
+                if (humedadOn) {
+                    sensorManager.unregisterListener(this, humedad);
+                    Toast.makeText(this, R.string.toast_humedadOFF, Toast.LENGTH_SHORT).show();
+                }else{
+                    sensorManager.registerListener(this, humedad, SensorManager.SENSOR_DELAY_NORMAL);
+                    Toast.makeText(this, R.string.toast_humedadON, Toast.LENGTH_SHORT).show();;
+                }
+                humedadOn = !humedadOn;
+                break;
+            case R.id.btnTemperatura:
+                if (temperaturaOn) {
+                    sensorManager.unregisterListener(this, temperatura);
+                    Toast.makeText(this, R.string.toast_temperaturaOFF, Toast.LENGTH_SHORT).show();;
+                }else{
+                    sensorManager.registerListener(this, temperatura, SensorManager.SENSOR_DELAY_NORMAL);
+                    Toast.makeText(this, R.string.toast_temperaturaON, Toast.LENGTH_SHORT).show();;
+                }
+                temperaturaOn = !temperaturaOn;
+                break;
+            case R.id.btnAcelerometro:
+                if (acelerometroOn) {
+                    sensorManager.unregisterListener(this, acelerometro);
+                    Toast.makeText(this, R.string.toast_aceleracionOFF, Toast.LENGTH_SHORT).show();;
+                }else{
+                    sensorManager.registerListener(this, acelerometro, SensorManager.SENSOR_DELAY_NORMAL);
+                    Toast.makeText(this, R.string.toast_aceleracionON, Toast.LENGTH_SHORT).show();;
+                }
+                acelerometroOn = !acelerometroOn;
+                break;
+            case R.id.btnIluminacion:
+                if (iluminacionOn) {
+                    sensorManager.unregisterListener(this, iluminacion);
+                    Toast.makeText(this, R.string.toas_ilumuncacionOFF, Toast.LENGTH_SHORT).show();;
+                }else{
+                    sensorManager.registerListener(this, iluminacion, SensorManager.SENSOR_DELAY_NORMAL);
+                    Toast.makeText(this, R.string.toas_ilumuncacionON, Toast.LENGTH_SHORT).show();;
+                }
+                iluminacionOn = !iluminacionOn;
+                break;
+            case R.id.btnLimite:
+                String s = editLimite.getText().toString();
+                LIMITE_ACELERACION = Double.parseDouble(s);
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.modoPrueba){
+            cambiarModoPrueba();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void cambiarModoPrueba(){
+        modoPrueba = !modoPrueba;
+
+        if (!modoPrueba){
+            btnLimite.setVisibility(View.INVISIBLE);
+            editLimite.setVisibility(View.INVISIBLE);
+        }else{
+            btnLimite.setVisibility(View.VISIBLE);
+            editLimite.setVisibility(View.VISIBLE);
+        }
     }
 }
